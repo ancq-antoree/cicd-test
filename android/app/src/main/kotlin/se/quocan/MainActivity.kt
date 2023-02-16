c9323a4be5ac8a975fbec1c5e8796654166dff3f
ancq-antoree <ancq@antoree.com>
2023-02-16 14:15:10 +0700

    initial commit

---
+++ android/app/src/main/kotlin/se/quocan/MainActivity.kt
@@ -0,0 +1,6 @@
+package se.quocan.cicd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:cicd/app/app.dart';
+import 'package:cicd/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
